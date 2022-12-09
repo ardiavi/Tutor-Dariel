59c040ef643975ce513a327be39ce4595532f713
ardiavi <ardiambis@gmail.com>
2022-12-09 14:28:00 +0700

    BISAAAAA tinggal rapihin

--- main.py
+++ main.py
@@ -83,29 +83,28 @@ def predict_csv(csv_file: UploadFile = File(...)):
 
 @app.post("/predict_loan")
 def predict(input: schemas.LenderData):
-    file = open("ml/training_nodel.joblib", "rb")
+    file = open("ml/train_model.joblib", "rb")
     trained_model = joblib.load(file)
     types = {
         "Dependents" : np.int32,
         "Education" : np.int32,
-        "ApplicationIncome" : np.int64,
+        "ApplicantIncome" : np.int64,
         "CoapplicantIncome" : np.float64,
         "LoanAmount" : np.int32,
         "Credit_History" : np.float64,
         "Property_Area" : np.int32,
-        "Loan_Status": np.int32,
         "Male" : np.uint8,
         "Self_Employed_Yes" : np.uint8,
         "married_Yes" : np.uint8,
     }
 
-    input_arr = [input.Dependents,input.Education,input.ApplicantIncome,input.CoapplicantIncome,input.LoanAmount, input.Credit_History, input.Property_Area, input.Self_Employed_Yes, input.married_Yes]
+    input_arr = [input.Dependents,input.Education,input.ApplicantIncome,input.CoapplicantIncome,input.LoanAmount, input.Credit_History, input.Property_Area, input.Male, input.Self_Employed_Yes, input.married_Yes]
     df = pd.DataFrame(input_arr).transpose()
     df.columns = list(types.keys())
     df = df.astype(types)
     df.to_numpy()
     
-    amount = df["LoanAmount"]
+    amount = int(df["LoanAmount"])
     results = (trained_model.predict(df))
     if results == 1:
         results = "Accepted"
